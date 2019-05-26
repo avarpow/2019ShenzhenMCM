411ab94c66b6aae19b893f60b0dfcdb9299734b5
Sunkle <luhw3@mail2.sysu.edu.cn>
2019-05-26 21:27:09 +0800

    上个版本忘了传脚本
    检验成对比较矩阵一致性的脚本
    版本日志也不想写了，后面再写吧

--- 5.24~5.26/wantCR.m
+++ 5.24~5.26/wantCR.m
@@ -1,9 +1,17 @@
-B1=[1 1/8 1/6;8 1 1;6 1 1]
-[V,D]=eig(B1);
+A=[1 1/4 4;4 1 8;1/4 1/8 1]
+%%
+[V,D]=eig(A);
 [x,y]=max(max(D));
-disp("λmax是否小于3.116？")
 disp("λmax=")
 disp(x)
+%%
+CI=(x-3)/2
+RI=0.58
+disp("CR是否小于0.1？")
+CR=CI/RI
 
+%%
 k=V(:,y);
+disp("标准化特征向量为：")
 k=k./sum(k)
+clear
